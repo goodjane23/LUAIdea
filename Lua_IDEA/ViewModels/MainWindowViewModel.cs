83c65cdfd2f834306cad07ee7d4b28642c8b00c4
goodjane23 <apochemubinet@gmail.com>
2023-05-28 19:12:33 +0300

    переделать под параметры

--- Lua_IDEA/ViewModels/MainWindowViewModel.cs
+++ Lua_IDEA/ViewModels/MainWindowViewModel.cs
@@ -13,6 +13,8 @@ using WinRT.Interop;
 using CommunityToolkit.Mvvm.Messaging;
 using Lua_IDEA.Messages;
 using System.IO;
+using Microsoft.UI.Xaml;
+using System.Resources;
 
 namespace Lua_IDEA.ViewModels;
 
@@ -207,6 +209,7 @@ public partial class MainWindowViewModel : ObservableObject, IRecipient<SelectRe
         };
 
         Tabs.Add(file);
+        SelectedTab = Tabs.Last();
     }
 
     private async Task GetFavoritesMacrosAsync()
@@ -263,39 +266,22 @@ public partial class MainWindowViewModel : ObservableObject, IRecipient<SelectRe
     [RelayCommand]
     private async void ShowTestMacro()
     {
-        var testMacro = new LuaFile()
-        {
-            Name = "Test Macro",
-            Content = "function M_XXX() \r\n\r\n local outputs_count = 8\r\n " +
-            "for i = 0, outputs_count  do \r\n" +
-            "if (PinGetState(Outputs.UserOutput_0 + i)) then\r\n DisplayMessage(\"Output_\"..i..\" ON\")\r\n else\r\n DisplayMessage(\"Output_\"..i..\" OFF\")\r\n end\r\n end end",
-            IsFavorite = false,
-            IsSaved = false,
-        };
-        Tabs.Add(testMacro);
-        SelectedTab = Tabs.Last();
 
     }
 
     [RelayCommand]
     private async void ShowHeightMap() 
     {
-        var testMacro = new LuaFile()
-        {
-            Name = "M155",
-            Content = "function m155()\r\n    local XWidth = 70\r\n    local YWidth = 50\r\n    local SafeZ  = 3\r\n    local ProbeZ = -3\r\n    local StepX  = 15\r\n    local StepY  = 15\r\n    local Feed   = 50\r\n    local TipHeight = 0\r\n    local ProbeFilename = \"C:\\\\temp\\\\probe.txt\"\r\n     \r\n    PushCurrentDistanceMode()\r\n    PushCurrentMotionMode()\r\n     \r\n    if (IsProbingPinConfigured()) then\r\n        -- open the file\r\n        file, msg = io.open(ProbeFilename, \"w\")\r\n         \r\n        if (file == nil) then\r\n            DisplayMessage(\"Could not open probe output file (\"..msg..\")\")\r\n            Stop()\r\n            return\r\n        end\r\n         \r\n        ExecuteMDI(\"F \"..Feed)\r\n        ExecuteMDI(\"G90 G38.2 Z-100\")\r\n         \r\n        -- set the current location to 0,0,0\r\n        ExecuteMDI(\"G92 X0Y0Z0\")\r\n        ExecuteMDI(\"G0 Z\"..SafeZ)\r\n         \r\n        local direction = 0\r\n        for y = 0, YWidth, StepY do\r\n            if (direction == 1) then\r\n                direction = 0\r\n            else\r\n                direction = 1\r\n            end\r\n             \r\n            for x = 0, XWidth, StepX do\r\n                if (direction == 1) then\r\n                    ExecuteMDI(\"G0 X\"..x..\" Y\"..y..\" Z\"..SafeZ)\r\n                else\r\n                    ExecuteMDI(\"G0 X\"..(XWidth - x)..\" Y\"..y..\" Z\"..SafeZ)\r\n                end\r\n                 \r\n                ExecuteMDI(\"G38.2 Z\"..ProbeZ)\r\n                LogCurrentPos(TipHeight)\r\n                ExecuteMDI(\"G0 Z\"..SafeZ)\r\n            end\r\n        end\r\n         \r\n        if (direction == 1) then\r\n            ExecuteMDI(\"G0 X\"..XWidth..\" Y\"..YWidth..\" Z\"..SafeZ)\r\n        else\r\n            ExecuteMDI(\"G0 X\"..\"0\"..\" Y\"..YWidth..\" Z\"..SafeZ)\r\n        end\r\n         \r\n        local HighZ = 5\r\n        ExecuteMDI(\"G0 Z\"..HighZ)\r\n        ExecuteMDI(\"G0 X0Y0\")\r\n         \r\n        file:close()\r\n    else\r\n        DisplayMessage(\"Probe input is not configured\")\r\n        return\r\n    end\r\nend\r\n \r\nfunction LogCurrentPos(tipHeight)\r\n    local CurrX = AxisGetPos(Axis.X)\r\n    local CurrY = AxisGetPos(Axis.Y)\r\n    local CurrZ = AxisGetPos(Axis.Z)\r\n     \r\n    local fmt = \"%.5f\"\r\n    file:write(string.format(fmt, CurrX)..\",\"..string.format(fmt, CurrY)..\",\"..string.format(fmt, CurrZ - tipHeight), \"\\n\")\r\nend",
-            IsFavorite = false,
-            IsSaved = false,
-        };
-        Tabs.Add(testMacro);
-        SelectedTab = Tabs.Last();
     }
 
-
-    private void OpenSelectedFile(string path)
+    [RelayCommand]
+    private async void ShowaAnalogInputsBOCommand()
     {
-
     }
 
+    [RelayCommand]
+    private async void ShowSpindleBOCommand()
+    {
+    }
 
 }
