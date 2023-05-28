066ba234f2e677199f520ea3cbbb64011fe03820
goodjane23 <apochemubinet@gmail.com>
2023-05-28 13:02:44 +0300

    add custom converter

---
+++ Lua_IDEA/Helpers/InverseBooleanConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Lua_IDEA.Helpers;
+
+public class InverseBooleanConverter : IValueConverter
+{
+    public InverseBooleanConverter()
+    {
+        
+    }
+    public object Convert(object value, Type targetType, object parameter,
+        System.Globalization.CultureInfo culture)
+    {
+        if (targetType != typeof(bool))
+            throw new InvalidOperationException("The target must be a boolean");
+
+        return !System.Convert.ToBoolean(value);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter,
+        System.Globalization.CultureInfo culture)
+    {
+        if (targetType != typeof(bool))
+            throw new InvalidOperationException("The target must be a boolean");
+
+        return !System.Convert.ToBoolean(value);
+    }
+}
+
