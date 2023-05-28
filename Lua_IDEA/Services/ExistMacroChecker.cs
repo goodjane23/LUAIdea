052b4ab475097618fa73e73c876319d31caf3a1a
goodjane23 <apochemubinet@gmail.com>
2023-05-28 17:30:18 +0300

    gett all files

--- Lua_IDEA/Services/ExistMacroChecker.cs
+++ Lua_IDEA/Services/ExistMacroChecker.cs
@@ -1,5 +1,6 @@
 ﻿using Lua_IDEA.Models;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.Win32;
 using MoonSharp.Interpreter;
 using System;
 using System.Collections.Generic;
@@ -13,25 +14,37 @@ namespace Lua_IDEA.Services;
 
 public class ExistMacroChecker
 {
+    private List<LuaFile> totalFiles = new();
+
     string error;
     public List<LuaFile> ExistMacros { get; set; } = new();
-    public List<LuaFile> ExistBackground { get; } = new();
+    public List<LuaFile> ExistBackground { get; set; } = new();
 
     public ExistMacroChecker()
     {
         var appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
         var pm = $@"{appDataPath}\Purelogic\Pumotix\Server\Macros\";
-        CreateDirectory(pm);
+        FindDirectory(pm);
     }
 
-    private async Task CreateDirectory(string pm)
+    private async Task FindDirectory(string directory)
     {
         try
         {
-            var rootDirectoryInfo = new DirectoryInfo(pm);
-            ExistMacros = await GetFiles(rootDirectoryInfo);
-            var node = await CreateDirectoryNode(rootDirectoryInfo);
+            //mainfolder - макросы основные лежат тут и пользовательские то же тут.
+            //Внутри должна быть папка с ФО
+            var rootDirectoryInfo = new DirectoryInfo(directory);
 
+            // список для внутренних папок
+            List<DirectoryInfo> folders = new List<DirectoryInfo>();
+
+            folders.AddRange(rootDirectoryInfo.GetDirectories().ToList());
+            folders.Add(rootDirectoryInfo);
+            foreach (var folder in folders)
+            {
+                 await GetFiles(folder);
+            }
+            await SortingMacro();
         }
         catch (Exception)
         {
@@ -39,10 +52,8 @@ public class ExistMacroChecker
         }
     }
 
-    private async Task<List<LuaFile>> GetFiles(DirectoryInfo path)
+    private async Task GetFiles(DirectoryInfo path)
     {
-        List<LuaFile> files = new List<LuaFile>();
-
         foreach (var file in path.GetFiles())
         {
             using var sr = new StreamReader(file.FullName);
@@ -71,17 +82,13 @@ public class ExistMacroChecker
                 Errors = error,
             };
 
-            files.Add(luaFile);
+            totalFiles.Add(luaFile);
         }
-        return files;
     }
-   
-    private async Task<List<DirectoryInfo>> CreateDirectoryNode(DirectoryInfo directoryInfo)
+
+    private async Task SortingMacro()
     {
-        List<DirectoryInfo> folders = new List<DirectoryInfo>();
-        var dInfo = directoryInfo.GetDirectories();
-
-        return dInfo.ToList();
-
+        ExistMacros = totalFiles.FindAll(x => x.Name.Contains(".pm"));
+        ExistBackground = totalFiles.FindAll(x => x.Name.Contains(".bm"));
     }
 }
